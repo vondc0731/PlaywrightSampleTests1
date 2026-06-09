696f4f2f00601e207d4fd38ba3e68cfc39742b8d
“vondc0731” <von.dc@Vons-MacBook-Air-2.local>
2026-06-09 18:53:14 +0800

    junit-test-results.xml

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['tml'],
+    ['list'],
+    //['dot'],
+    //['json', { outputFile: 'json-test-results.json' }],
+    ['junit', { outputFile: 'junit-test-results.xml' }],
+    ['allure-playwright'],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
